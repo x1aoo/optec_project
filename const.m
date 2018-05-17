cc4ec30583313c15966a4c9d021e2aaf07787a42
xiaoo <mapeleaf@126.com>
2018-05-17 16:12:39 +0200

    make it correct

--- const.m
+++ const.m
@@ -1,7 +1,17 @@
 function [g h] = const(x)
-    global r1 d
+    global r1 C1 A B
     % g is vector of inequally of constrains 不等式
     % h is vector of equally of constrains 等式
+    
+    M=[A;x;B];
+    n=0:0.01:1;
+    d=zeros();
+    for i=1:length(M)-1
+        for j=1:length(n)
+            point=M(i,:)+n(j)*(M(i+1,:)-M(i,:));
+            d(j,i)=norm(point-C1);
+        end
+    end
     h = [];
     g(1) = r1-min(d(:,1));
     g(2) = r1-min(d(:,2));
--- obj.m
+++ obj.m
@@ -1,15 +1,5 @@
 function f = obj(x)
-global A B C1 d P
-P=[x(1) x(2)];
-M=[A;P;B];
-n=0:0.01:1;
-d=zeros();
-for i=1:length(M)-1
-    for j=1:length(n)
-        point=M(i,:)+n(j)*(M(i+1,:)-M(i,:));
-        d(j,i)=norm(point-C1);
-    end
-end
-f=norm(A-P)+norm(P-B);
+global A B 
+f=norm(A-x)+norm(x-B);
 % f=-f;
 end
